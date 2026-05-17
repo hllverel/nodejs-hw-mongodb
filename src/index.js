1b0374de98c0adad67b9b72f55e77761dc0516de
hllverel <hllozdemir@live.com>
2026-05-17 18:09:18 +0300

    hw2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import setupServer from "./server";
+import { initMongoConnection } from "./db/initMongoConnection";
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
