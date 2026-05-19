fad46876084cd11cefd966b935830cee35b1f7b5
hllverel <hllozdemir@live.com>
2026-05-19 23:30:17 +0300

    minor fix

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { getAllContacts, getContactById } from './services/contacts.js';
 
 export default function setupServer() {
   const app = express();
-  const PORT = process.env.PORT || 3000;;
+  const PORT = process.env.PORT || 3000;
 
   app.use(cors());
 
