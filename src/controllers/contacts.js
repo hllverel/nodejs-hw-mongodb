3597fae9e6dee4b3b626c6e0176d7b1b79e78ffe
hllverel <hllozdemir@live.com>
2026-05-22 00:06:17 +0300

    minor update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -31,7 +31,7 @@ export const createContactController = async (req, res) => {
     const contact = await createContact(req.body);
     res.status(201).json({
         status: 201,
-        message: 'Successfully created a contact',
+        message: 'Successfully created a contact!',
         data: contact,
     });
 };
