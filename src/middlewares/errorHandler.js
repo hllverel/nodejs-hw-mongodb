4dea0c62e9299be12987c8139fbcdfcc9d162811
hllverel <hllozdemir@live.com>
2026-05-22 00:01:52 +0300

    updated error handlers

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -10,8 +10,9 @@ export const errorHandler = (err, req, res, next) => {
         return;
     }
 
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
+    res.status(500).json({
+        status: 500,
+        message: 'Something went wrong',
+        data: err.message,
+    });
 };
