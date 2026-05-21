f803e8b00f4e2609019b9de9a2ba44d0ba632c8d
hllverel <hllozdemir@live.com>
2026-05-21 23:46:13 +0300

    draft

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export default function setupServer() {
   const app = express();
   const PORT = process.env.PORT || 3000;
 
+  app.use(express.json());
   app.use(cors());
 
   app.use(pino({
@@ -16,43 +19,10 @@ export default function setupServer() {
     },
   }));
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(contactsRouter);
 
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: "Contact not found",
-      });
-      return;
-    };
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use((req, res) => {
-    res.status(404).json({
-        message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({ message: 'Internal server error' });
-  });
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
