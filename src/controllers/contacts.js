466d9eed9a3c8010dd7712ea24bf82a35e6e4980
hllverel <hllozdemir@live.com>
2026-05-25 21:07:17 +0000

    sorting, pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,8 +1,13 @@
 import createHttpError from 'http-errors';
 import { getAllContacts, getContactById, createContact, deleteContact, updateContact } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getContactsController = async (req, res) => {
-    const contacts = await getAllContacts();
+    const { page, perPage } = parsePaginationParams(req.query);
+    const { sortBy, sortOrder} = parseSortParams(req.query);
+
+    const contacts = await getAllContacts({page, perPage, sortBy, sortOrder});
 
     res.status(200).json({
         status: 200,
