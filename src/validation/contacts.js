25ad5249ba909dfe6485d6093b601f204ac7c24d
hllverel <hllozdemir@live.com>
2026-05-28 18:31:11 +0300

    validation fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -9,19 +9,19 @@ export const createContactSchema = Joi.object({
         }),
     phoneNumber: Joi.string().min(3).max(20).required(),
     email: Joi.string().min(3).max(20),
-    isFavourite: Joi.boolean,
+    isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20).required().messages({
+    name: Joi.string().min(3).max(20).messages({
         'string.base': 'Name should be a string',
         'string.min': 'Name should have at least {#limit} characters',
         'string.max': 'Name should have at most {#limit} characters',
         'any.required': 'Name is required',
         }),
-    phoneNumber: Joi.string().min(3).max(20).required(),
+    phoneNumber: Joi.string().min(3).max(20),
     email: Joi.string().min(3).max(20),
-    isFavourite: Joi.boolean,
-    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
 });
