be0c68668059c7a11329cbf7d5a2ebdd73280be7
hllverel <hllozdemir@live.com>
2026-06-10 22:21:08 +0000

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { getContactsController, getContactByIdController, createContactController, deleteContactController, upsertContactController, patchContactController } from "../controllers/contacts.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const contactsRouter = Router();
 
@@ -12,12 +13,15 @@ contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 contactsRouter.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 contactsRouter.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-// contactsRouter.post('/register', validateBody(), ctrlWrapper());
 
 contactsRouter.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 contactsRouter.put('/contacts/:contactId', isValidId, ctrlWrapper(upsertContactController));
 
 contactsRouter.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+
+contactsRouter.use(authenticate);
+
+contactsRouter.get('/', ctrlWrapper(getContactsController));
 
 export default contactsRouter;
