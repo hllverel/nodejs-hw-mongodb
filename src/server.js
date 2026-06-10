da3f02aa56a928767ad7501e6088dc0b8248efce
hllverel <hllozdemir@live.com>
2026-06-10 22:09:35 +0000

    user login and logout

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -27,4 +28,12 @@ export default function setupServer() {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
+};
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+};
