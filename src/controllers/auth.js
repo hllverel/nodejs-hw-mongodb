20ddab8a291c3df8de7538a89a57a8f07d67816d
hllverel <hllozdemir@live.com>
2026-06-21 21:56:08 +0300

    installed nodemailer, handlebars and jwt, added draft of password reset functionality

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-import { registerUser, loginUser, logoutUser, refreshUsersSession } from "../services/auth.js";
+import { registerUser, loginUser, logoutUser, refreshUsersSession, requestResetToken, resetPassword } from "../services/auth.js";
 import {THIRTY_DAYS} from "../constants/index.js";
 
 export const registerUserController = async (req, res) => {
@@ -69,5 +69,23 @@ export const refreshUserSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
   });
 };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { registerUserSchema, loginUserSchema } from "../validation/auth.js";
-import { registerUserController, loginUserController, logoutUserController, refreshUserSessionController } from "../controllers/auth.js";
+import { registerUserSchema, loginUserSchema, requestResetEmailSchema, resetPasswordSchema } from "../validation/auth.js";
+import { registerUserController, loginUserController, logoutUserController, refreshUserSessionController, requestResetEmailController, resetPasswordController } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 
 const authRouter = Router();
@@ -10,5 +10,7 @@ authRouter.post('/register', validateBody(registerUserSchema), ctrlWrapper(regis
 authRouter.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
 authRouter.post('/logout', ctrlWrapper(logoutUserController));
 authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
+authRouter.post('/request-reset-email', validateBody(requestResetEmailSchema), ctrlWrapper(requestResetEmailController));
+authRouter.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
 
 export default authRouter;
