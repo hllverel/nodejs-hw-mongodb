083ddbf02ca8705149e30335d7655152252dfae8
hllverel <hllozdemir@live.com>
2026-06-21 16:40:05 +0000

    fixed contacts routing and controllers

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -9,7 +9,7 @@ export const getContactsController = async (req, res) => {
     const { sortBy, sortOrder} = parseSortParams(req.query);
     const filter = parseFilterParams(req.query);
 
-    const contacts = await getAllContacts({page, perPage, sortBy, sortOrder, filter});
+    const contacts = await getAllContacts({page, perPage, sortBy, sortOrder, filter, userId: req.user._id});
 
     res.status(200).json({
         status: 200,
@@ -20,7 +20,7 @@ export const getContactsController = async (req, res) => {
 
 export const getContactByIdController = async (req, res, next) => {
     const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+    const contact = await getContactById(contactId, req.user._id);
 
     if (!contact) {
         throw createHttpError(404, 'Contact not found');
@@ -34,7 +34,7 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
-    const contact = await createContact(req.body);
+    const contact = await createContact(req.body, req.user._id);
     res.status(201).json({
         status: 201,
         message: 'Successfully created a contact!',
@@ -44,7 +44,7 @@ export const createContactController = async (req, res) => {
 
 export const deleteContactController = async (req, res, next) => {
     const { contactId } = req.params;
-    const contact = await deleteContact(contactId);
+    const contact = await deleteContact(contactId, req.user._id);
 
     if (!contact) {
         next(createHttpError(404, 'Contact not found'));
@@ -56,7 +56,7 @@ export const deleteContactController = async (req, res, next) => {
 
 export const upsertContactController = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body, { upsert: true });
+    const result = await updateContact(contactId, req.body, { upsert: true }, req.user._id);
 
     if (!result) {
         next(createHttpError(404, 'Contact not found'));
@@ -74,7 +74,7 @@ export const upsertContactController = async (req, res, next) => {
 
 export const patchContactController = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body);
+    const result = await updateContact(contactId, req.body, { }, req.user._id);
 
     if (!result) {
         next(createHttpError(404, 'Contact not found'));
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -10,19 +10,15 @@ import { authenticate } from "../middlewares/authenticate.js";
 const contactsRouter = Router();
 contactsRouter.use(authenticate);
 
-contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
-contactsRouter.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
-
-contactsRouter.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-
-contactsRouter.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
-
-contactsRouter.put('/contacts/:contactId', isValidId, ctrlWrapper(upsertContactController));
-
-contactsRouter.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
-
-contactsRouter.use(authenticate);
-
 contactsRouter.get('/', ctrlWrapper(getContactsController));
+contactsRouter.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
+contactsRouter.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+
+contactsRouter.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+
+contactsRouter.put('/:contactId', isValidId, ctrlWrapper(upsertContactController));
+
+contactsRouter.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
 export default contactsRouter;
