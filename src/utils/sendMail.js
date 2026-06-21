21395e6ce73fc9127e245736711a9310a5be6121
hllverel <hllozdemir@live.com>
2026-06-21 22:07:12 +0300

    minor update

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,7 +3,8 @@ import { SMTP } from '../constants/index.js';
 
 const transporter = nodemailer.createTransport({
   host: process.env[SMTP.SMTP_HOST],
-  port: Number(process.env[SMTP.SMTP_PORT]),
+    port: Number(process.env[SMTP.SMTP_PORT]),
+  secure: false,
   auth: {
     user: process.env[SMTP.SMTP_USER],
     pass: process.env[SMTP.SMTP_PASSWORD],
