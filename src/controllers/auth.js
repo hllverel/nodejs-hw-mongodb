f74fd8612e0e93ba804eea535da17aad7d22d285
hllverel <hllozdemir@live.com>
2026-07-17 23:12:03 +0300

    added auth endpoint docs

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -27,7 +27,7 @@ export const loginUserController = async (req, res) => {
 
   res.status(200).json({
     status: 200,
-    message: 'Successfully logged in an user!',
+    message: 'Successfully logged in as a user!',
     data: {
       accessToken: session.accessToken,
     },
